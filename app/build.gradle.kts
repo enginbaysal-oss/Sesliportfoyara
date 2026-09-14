fae17457fbdd856a97dab731750e8e9839075b84
Engin Baysal <enginbaysal@example.com>
2026-09-14 21:44:17 +0300

    Build: Bump versionCode to 5 and versionName to 1.2.3 to force device deployment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,8 +88,8 @@ android {
         applicationId = "com.example.sesliportfoyara"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.3"
     }
     packaging {
         resources {
